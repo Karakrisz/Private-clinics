93196383a739ffb7c12ca10f92831c5d1e087648
Scwartz Krisztián <webevolution13@gmail.com>
2019-01-12 22:37:18 +0100

    ok

---
+++ js/my.js
@@ -0,0 +1,17 @@
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+// menu
+$(document).ready(function(){
+  $(".bar").click(function(){
+    $(".topnav").slideToggle("slow");
+  });
+});
+// exchanges
+$(window).resize(function() {
+  if ($(window).width() >= 970) {
+    $(".topnav").fadeIn();
+  } else {
+    $(".topnav").hide();
+  }
+});
